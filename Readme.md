89aa71f0bac92b4edf4fff613be3021f60d353bd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# MonthCellControl - a custom style example
+
+
+This example defines a custom <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfSchedulingVisualMonthCellControltopic">MonthCellControl</a> style which specifies the font size and format to display a date in a <a href="http://help.devexpress.com/#WPF/CustomDocument119207">MonthView</a> cell.<br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/monthcellcontrol-a-custom-style-example-t606303/17.2.3+/media/9836ccd3-bac6-4995-a383-1976ec29a0c1.png">
+
+<br/>
+
+
---
+++ VB/CellControlExample/ViewModels/MyViewModel.vb
@@ -0,0 +1,31 @@
+﻿Imports CellControlExample.Model
+Imports DevExpress.Mvvm.DataAnnotations
+Imports System
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Media
+
+Namespace CellControlExample.ViewModels
+    <POCOViewModel> _
+    Public Class MyViewModel
+        Public Overridable Property Categories() As ObservableCollection(Of MyCategory)
+        Protected Sub New()
+            Categories = CreateCategories()
+            UseCustomCellControlStyle = True
+        End Sub
+
+        #Region "#CreateCategories"
+        Private Function CreateCategories() As ObservableCollection(Of MyCategory)
+            Dim result As New ObservableCollection(Of MyCategory)()
+            result.Add(MyCategory.Create(0, "White category", Colors.White))
+            result.Add(MyCategory.Create(1, "Orange category", Colors.Orange))
+            result.Add(MyCategory.Create(2, "Red category", Colors.Red))
+            Return result
+        End Function
+        #End Region ' #CreateCategories
+        Public Overridable Property UseCustomCellControlStyle() As Boolean
+    End Class
+End Namespace
